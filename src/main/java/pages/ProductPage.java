bd5fcfd10d5dd16ada2ffc43b007f605b9ee723d
DoaaTawfik25 <doaa.25tawfik@gmail.com>
2025-05-09 11:38:09 +0300

    uploading Product Page with two basic functionalities to add product and remove it to/from cart

--- src/main/java/pages/ProductPage.java
+++ src/main/java/pages/ProductPage.java
@@ -1,6 +1,8 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class ProductPage extends PageBase {
 
@@ -8,7 +10,22 @@ public class ProductPage extends PageBase {
         super(driver);
     }
 
+    final String[] productsName = new String[]{
+            "sauce-labs-backpack",
+            "sauce-labs-bike-light",
+            "sauce-labs-bolt-t-shirt",
+            "sauce-labs-fleece-jacket",
+            "sauce-labs-onesie",
+            "test.allthethings()-t-shirt-(red)"
+    };
 
+    public void AddProductToCartById(int productId) throws InterruptedException {
+        By productById = By.id("add-to-cart-" + productsName[productId]);
+        ClickButton(productById);
+    }
 
-
+    public void RemoveProductFromCartById(int productId) throws InterruptedException {
+        By productById = By.id("remove-" + productsName[productId]);
+        ClickButton(productById);
+    }
 }
