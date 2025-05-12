fb36d616b33fb84f577c7a324b59874c6bc913b2
Anas Elbaioomy <111653305+13-ctrl@users.noreply.github.com>
2025-05-12 14:28:04 +0300

    Update TestBase.java

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,6 +2,8 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -11,14 +13,13 @@ import pages.PageBase;
 public class TestBase {
 
     public static WebDriver driver;
-
     @BeforeMethod
     public void OpenBrowser() {
-        driver = new ChromeDriver();
+        driver = new EdgeDriver();
         driver.get("https://www.saucedemo.com/");
         driver.manage().window().maximize();
     }
-
+    
     @AfterMethod
     public void CloseBrowser() {
         driver.close();
